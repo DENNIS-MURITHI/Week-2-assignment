2fc6e09890c102179db0f27332eed2baf9ce40f9
dennis.muthuri <denmurithi@gmail.com>
2023-10-15 15:13:06 +0300

    Week 2 assignment

---
+++ Question.js
@@ -0,0 +1,13 @@
+//Question 1:
+let numberStr = "42";
+console.log(Number(numberStr));//"42" been conerted to a number,42, simply because the Number() type conversion has been used to convert it from string to number.
+
+// Question 2:
+let num = 2024;
+console.log(String(num));//"2024". The String() function takes in num as the parameter and converts it to string.
+
+//question 3:
+let truthyValue ="Hello";
+let falsyValue = 0;
+console.log(Boolean(truthyValue));//true- Boolean outputs truthy or falsy. Truth means 1 / a string with a vallue.
+console.log(Boolean(falsyValue));//false- Boolean outputs truthy or falsy. Falsy means zero / empty string.
